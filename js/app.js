431ff6ede91c1e57c23dcff1125fa26ed5144d50
Mon <toreachmansi@gmail.com>
2017-08-09 12:30:16 +1000

    - Refactored and added comments

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-(function() {
+(function () {
 
-    'use strict'
+    'use strict';
 
     var ENTER = 13;
     var STR_KEY = "notes";
@@ -16,57 +16,34 @@
         this.init();
 
     }
+
     var proto = ToDoApp.prototype;
 
-    proto.storageHelper = {
-
-        getItemIndById: function(id, allNotes) {
-            for (var i = 0; i < allNotes.length; i++) {
-                if (allNotes[i].id == id) {
-                    return i;
-                }
-            }
-        },
-
-        setItemInStore: function(key, value) {
-            localStorage.setItem(key, value);
-        },
-
-        getNotesFromStore: function(localStore, key) {
-
-            return localStore && localStore.getItem(key) && JSON.parse(localStore.getItem(key)) || [];
-        },
-        updateStorage: function(allNotes, id, isRemove, isComplete) {
-            var i = this.getItemIndById(id, allNotes);
-            if (isRemove) {
-                allNotes.splice(i, 1);
-            } else {
-                allNotes[i].completed = isComplete;
-            }
-            localStorage.setItem(STR_KEY, JSON.stringify(allNotes));
-        }
-    };
-
-    proto.init = function() {
-        var nts = this.storageHelper.getNotesFromStore(this.store, STR_KEY)
+    /**
+     * Checks if there are any notes in localstorage and if present, renders them
+     */
+    proto.init = function () {
+        var nts = this.storageHelper().getNotesFromStore(this.store, STR_KEY);
         this.storedNotes = nts;
         if (nts) {
             for (var i = 0; i < nts.length; i++) {
                 this.list.append(this.createNode(this.storedNotes[i]));
             }
         }
-    }
+    };
 
-    proto.filterByState = function(event) {
+    /**
+     * shows items in a particular state: all, complete or incomplete
+    */
+    proto.filterByState = function (event) {
 
         var allItems = Array.prototype.slice.call(this.list.children('li'));
         var btn = event.target;
         var action = btn.id;
 
-        $('#noteList li').removeClass("hidden");
-        $('#filterDiv button').removeClass('btn-secondary').addClass('btn-info');
+        $('#noteList').find('li').removeClass("hidden");
+        $('#filterDiv').find('button').removeClass('btn-secondary').addClass('btn-info');
         $(btn).removeClass('btn-info').addClass('btn-secondary');
-
 
         for (var i = 0; i < allItems.length; i++) {
             var anItem = (allItems[i]);
@@ -78,26 +55,11 @@
         }
     };
 
-    proto.toggleItem = function(event) {
-        var clickedItm = event.target;
-        var liItem = $(clickedItm).closest("li");
-
-        liItem && liItem.toggleClass("completed");
-        this.storageHelper.updateStorage(this.storedNotes, $(liItem).attr("id"), false, $(liItem).hasClass("completed"));
-
-    };
-
-
-    proto.deleteItem = function(event) {
-        var clickedItm = event.target;
-        var liItem = $(clickedItm).closest("li");
-
-        this.storageHelper.updateStorage(this.storedNotes, $(liItem).attr("id"), true, null);
-        liItem && liItem.remove();
-
-    };
-
-    proto.insertItem = function(event) {
+     /**
+     * Captures key press event on text box and
+     *  calls createNode to add new item to list
+     */
+    proto.insertItem = function (event) {
         var element = event.target;
         var text = element.value.trim();
         if (text && event.keyCode === ENTER) {
@@ -108,45 +70,111 @@
             };
             this.storedNotes.push(item);
             this.list.append(this.createNode(item));
-            this.storageHelper.setItemInStore("notes", JSON.stringify(this.storedNotes));
+            this.storageHelper().setItemInStore(STR_KEY, this.storedNotes);
             element.value = '';
         }
     };
-    proto.createNode = function(item) {
+
+    /**
+     * creates a new html node for a new item
+     */
+    proto.createNode = function (item) {
         var isItmComplete = item.completed;
+
         var node = $('<li/>')
             .attr("id", item.id)
             .toggleClass('completed', isItmComplete);
-        var div = $('<div/>');
+
         var chk = $('<input/>')
             .attr("type", "checkbox")
-            .prop("checked", isItmComplete)
-            .addClass("toggleItem");
-        var sp = $("<span/>")
-            .text(item.text);
+            .prop("checked", isItmComplete);
 
-        var del = $("<i/>")
-            .addClass("fa fa-trash-o delItem");
+        var spn = $("<span/>").text(item.text);
+        var del = $("<i/>").addClass("fa fa-trash-o delItem");
+        var div = $('<div/>');
 
         div.append(chk);
-        div.append(sp);
+        div.append(spn);
         div.append(del);
-
         node.append(div);
 
-
         return node;
+    };
+
+    /**
+     * marks items as complete or incomplete
+     */
+    proto.toggleItem = function (event) {
+        var clickedItm = event.target;
+        var liItem = $(clickedItm).closest("li");
+
+        liItem && liItem.toggleClass("completed");
+        this.storageHelper().updateStorage(this.storedNotes, $(liItem).attr("id"), false, $(liItem).hasClass("completed"));
 
     };
-    proto.addEventListeners = function() {
+
+
+    proto.deleteItem = function (event) {
+        var clickedItm = event.target;
+        var liItem = $(clickedItm).closest("li");
+
+        this.storageHelper().updateStorage(this.storedNotes, $(liItem).attr("id"), true, null);
+        liItem && liItem.remove();
+
+    };
+
+
+    /**
+     * Event listeners for various user actions
+     */
+    proto.addEventListeners = function () {
         this.insert.on("keypress", this.insertItem.bind(this));
-        this.list.on("click", ".toggleItem", this.toggleItem.bind(this));
+        this.list.on("click", "input[type=checkbox]", this.toggleItem.bind(this));
         this.list.on("click", "i", this.deleteItem.bind(this));
         this.filterDiv.on("click", "button", this.filterByState.bind(this));
 
     };
 
-    $(document).ready(function() {
+
+    /**
+     * Storage Helper - to fetch/store items to and from localstorage
+     */
+    proto.storageHelper = function () {
+        var getItemIndById = function (id, allNotes) {
+                for (var i = 0; i < allNotes.length; i++) {
+                    if (allNotes[i].id == id) {
+                        return i;
+                    }
+                }
+            },
+            setItemInStore = function (key, value) {
+                localStorage.setItem(key,  JSON.stringify(value));
+            },
+            getNotesFromStore = function (localStore, key) {
+
+                return localStore && localStore.getItem(key) && JSON.parse(localStore.getItem(key)) || [];
+            },
+            updateStorage = function (allNotes, id, isRemove, isComplete) {
+                var i = getItemIndById(id, allNotes);
+                if (isRemove) {
+                    allNotes.splice(i, 1);
+                } else {
+                    allNotes[i].completed = isComplete;
+                }
+                localStorage.setItem(STR_KEY, JSON.stringify(allNotes));
+            };
+
+        return {
+            setItemInStore: setItemInStore,
+            getNotesFromStore: getNotesFromStore,
+            updateStorage: updateStorage
+
+        };
+
+    };
+
+
+    $(document).ready(function () {
 
         var app = new ToDoApp(localStorage);
     });
